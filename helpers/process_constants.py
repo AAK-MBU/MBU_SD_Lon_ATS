6517709f7e1381276ec640a3e2ec21703fe9cdbe
Sebastian <ahss@aarhus.dk>
2026-03-20 13:44:57 +0100

    Change way of getting procargs from sys arg

--- helpers/process_constants.py
+++ helpers/process_constants.py
@@ -16,24 +16,21 @@ def _load_constants():
         return
 
     with RPAConnection(db_env="PROD", commit=False) as conn:
-        _cached_constants["FaellesDbConnectionString"] = os.getenv("DBCONNECTIONSTRINGFAELLESSQL")
+        _cached_constants["FaellesDbConnectionString"] = os.getenv(
+            "DBCONNECTIONSTRINGFAELLESSQL"
+        )
 
-        _cached_constants["DBCONNECTIONSTRINGPROD"] = os.getenv("DBCONNECTIONSTRINGPROD")
+        _cached_constants["DBCONNECTIONSTRINGPROD"] = os.getenv(
+            "DBCONNECTIONSTRINGPROD"
+        )
 
-        if "--kv1" in sys.argv:
-            _cached_constants["kv_proc_args"] = json.loads(conn.get_constant("sdloen_kv1_procargs").get("value", "{}"))
+        # Determine which --kvX flag is present
+        kv_flag = next((arg for arg in sys.argv if arg.startswith("--kv")), None)
 
-        elif "--kv2" in sys.argv:
-            _cached_constants["kv_proc_args"] = json.loads(conn.get_constant("sdloen_kv2_procargs").get("value", "{}"))
-
-        elif "--kv3" in sys.argv:
-            _cached_constants["kv_proc_args"] = json.loads(conn.get_constant("sdloen_kv3_procargs").get("value", "{}"))
-
-        elif "--kv4" in sys.argv:
-            _cached_constants["kv_proc_args"] = json.loads(conn.get_constant("sdloen_kv4_procargs").get("value", "{}"))
-
-        elif "--kv5" in sys.argv:
-            _cached_constants["kv_proc_args"] = json.loads(conn.get_constant("sdloen_kv5_procargs").get("value", "{}"))
+        if kv_flag:
+            key = f"sdloen_{kv_flag[2:]}_procargs"  # "--kv3" -> "sdloen_kv3_procargs"
+            value = conn.get_constant(key).get("value", "{}")
+            _cached_constants["kv_proc_args"] = json.loads(value)
 
         _cached_constants["e-mail_noreply"] = conn.get_constant("e-mail_noreply").get("value", "")
 
